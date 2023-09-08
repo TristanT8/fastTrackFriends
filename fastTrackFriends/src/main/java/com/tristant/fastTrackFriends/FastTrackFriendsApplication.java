58b66bb813f8373ff862236255bb8b0aa62ab517
TristanT8 <115902705+TristanT8@users.noreply.github.com>
2023-09-08 13:13:22 -0400

    Add files via upload

---
+++ fastTrackFriends/src/main/java/com/tristant/fastTrackFriends/FastTrackFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.tristant.fastTrackFriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastTrackFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastTrackFriendsApplication.class, args);
+	}
+
+}
---
+++ fastTrackFriends/src/test/java/com/tristant/fastTrackFriends/FastTrackFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tristant.fastTrackFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastTrackFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
