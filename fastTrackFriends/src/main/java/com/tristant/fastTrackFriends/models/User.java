0a37ed19aee5f36bbcca8767d8459f9bdf200c15
TristanT8 <115902705+TristanT8@users.noreply.github.com>
2023-09-10 13:19:08 -0400

    Add files via upload

--- fastTrackFriends/src/main/java/com/tristant/fastTrackFriends/models/User.java
+++ fastTrackFriends/src/main/java/com/tristant/fastTrackFriends/models/User.java
@@ -1,10 +1,38 @@
 package com.tristant.fastTrackFriends.models;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String username;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	private String confirmPassword;
+	
+	@Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+	
 }
