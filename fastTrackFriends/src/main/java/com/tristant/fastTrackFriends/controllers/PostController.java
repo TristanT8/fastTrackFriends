271d7eaffa9a353121ca6edd7a73d5a26b9f376a
TristanT8 <115902705+TristanT8@users.noreply.github.com>
2023-09-21 13:25:23 -0400

    Add files via upload

--- fastTrackFriends/src/main/java/com/tristant/fastTrackFriends/controllers/PostController.java
+++ fastTrackFriends/src/main/java/com/tristant/fastTrackFriends/controllers/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tristant.fastTrackFriends.models.Post;
@@ -60,8 +61,37 @@ public class PostController {
 		}
 	}
 	
+	@PutMapping("/update/{id}")
+	public String updatePost(@Valid @ModelAttribute("post") Post post, BindingResult result) {
+		if(result.hasErrors()) {
+			return "editPost.jsp";
+		} else {
+			this.postServ.updatePost(post);
+			return "redirect:/home";
+		}
+	}
 	
+	@GetMapping("/view/{postId}")
+	public String viewPost(@PathVariable("postId") Long postId, Model model) {
+		Post post = postServ.findById(postId);
+		model.addAttribute("post", post);
+		return "viewPost.jsp";
+	}
 	
+	@GetMapping("/delete/{postId}")
+	public String deletePost(@PathVariable("postId") Long postId) {
+		this.postServ.deletePost(postId);
+		return "redirect:/home";
+	}
+	
+	@GetMapping("/return/home")
+	public String home(Model viewModel, HttpSession session) {
+		if(session.getAttribute("userId") == null) {
+			return "redirect:/";
+		}
+		viewModel.addAttribute("currentUser", uServ.findById((Long) session.getAttribute("userId")));
+		return "redirect:/home";
+	}
 	
 	
 }
